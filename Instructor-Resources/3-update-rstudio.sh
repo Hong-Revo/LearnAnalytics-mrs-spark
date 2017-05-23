a2707afae0f86dc546039130c803d46f0c2c56b8
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-05-23 09:54:58 -0700

    Add version to update-rstudio script

--- Instructor-Resources/3-update-rstudio.sh
+++ Instructor-Resources/3-update-rstudio.sh
@@ -2,4 +2,4 @@
 
 wget https://raw.githubusercontent.com/akzaidi/etc/master/inst/install-rstudio-ubuntu.sh
 chmod +x ./install-rstudio-ubuntu.sh
-./install-rstudio-ubuntu.sh
+./install-rstudio-ubuntu.sh 1.1.240
