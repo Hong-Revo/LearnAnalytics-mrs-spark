31540874475da99d1addc050af9e1e58f3b07568
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-06-13 20:42:02 -0700

    Update setup script with rstudio-server update

--- Instructor-Resources/setup.sh
+++ Instructor-Resources/setup.sh
@@ -38,3 +38,8 @@ hdfs dfs -copyFromLocal Weather hdfs://mycluster/share
 
 Revo64 -e 'install.packages(c("sparklyr", "ggplot2", "tidyr"), repos = "https://mran.microsoft.com/snapshot/2017-05-01")'
 
+# update rstudio-server
+
+wget https://raw.githubusercontent.com/akzaidi/etc/master/inst/install-rstudio-ubuntu.sh
+chmod +x ./install-rstudio-ubuntu.sh
+./install-rstudio-ubuntu.sh 1.1.267
